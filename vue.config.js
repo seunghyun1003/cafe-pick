efdae5cc08456635494e162f01c251a721b4a745
seunghyun1003 <s961003-@naver.com>
2021-02-22 01:24:35 +0900

    publicPath경로 재설정

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
   publicPath: '/cafe-pick/',
-  outputDir: './cafe-pick',
-=======
-  outputDir: './cafe-pick',
-  publicPath: '/cafe-pick/'
->>>>>>> f3e93e674d60fd5ca38976906172489a11779643
   transpileDependencies: [
     'vuetify'
   ]
